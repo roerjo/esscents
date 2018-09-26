833485df4e5209e0ab31e862a76aadf28efdd56b
Eric Johnson <roerjo.personal@gmail.com>
2018-09-25 22:17:48 -0400

    databases setup|sqlite memory testing db

---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
